ab663d91d6b8533f67c7f74c487830b5ea142885
Yau <s0586941@htw-berlin.de>
2025-08-07 14:20:31 +0200

    License Header added in every source code

--- src/main/java/com/example/SimpleSentence.java
+++ src/main/java/com/example/SimpleSentence.java
@@ -1,3 +1,22 @@
+/*
+ * Diese Datei ist Teil von My-Prose Anwendung.
+ * 
+ * Copyright (c) [2025] [https://github/mileyCg]
+ * 
+ * Lizenziert unter der Open Software License Version 3.0 (OSL-3.0);
+ * diese Datei darf nur in Übereinstimmung mit der Lizenz verwendet werden.
+ * 
+ * Eine Kopie der Lizenz ist verfügbar unter:
+ *   https://opensource.org/licenses/OSL-3.0 
+ *   
+ * Sofern nicht gesetzlich vorgeschrieben oder schriftlich vereinbart,
+ * wird die Software unter der Lizenz "WIE BESEHEN" bereitgestellt –  
+ * OHNE JEGLICHE GEWÄHRLEISTUNG, weder ausdrücklich noch stillschweigend.
+ * Siehe die Lizenz für spezifische Regelungen zu Rechten und Einschränkungen.
+ * 
+ * SPDX-License-Identifier: OSL-3.0
+ */
+
 package com.example;
 
 import de.htw_berlin.fb4.ossd.prose.Sentence;
